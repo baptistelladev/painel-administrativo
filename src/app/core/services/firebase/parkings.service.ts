b6764e5d6413b60ba300f5a45f5449366f64f380
baptistelaldev <vieirabaptistellafelipe@gmail.com>
2024-10-16 01:13:38 -0300

    criando dashboard

---
+++ src/app/core/services/firebase/parkings.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { addDoc, collection, Firestore } from '@angular/fire/firestore';
+import { IShortParking } from 'src/app/shared/models/IParking';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ParkingsService {
+
+  constructor(
+    private firestore : Firestore
+  ) { }
+
+  public async addDoc(collectionName: string, parking: IShortParking): Promise<any> {
+    try {
+      const docRef = await addDoc(collection(this.firestore, collectionName), parking);
+      console.log("Documento escrito com ID: ", docRef.id);
+      return docRef
+    } catch (e) {
+      console.error("Erro ao adicionar documento: ", e);
+    }
+  }
+}
